e7a3a92407a5aaaa24d00cbbb0b2425756298808
abhinavv62 <abhinavprajapati0718@gmail.com>
2026-02-13 11:38:48 +0530

    Update problem16.py

--- ASSIGNMENT-HOMEWORK/ASSIGNMENT4/problem16.py
+++ ASSIGNMENT-HOMEWORK/ASSIGNMENT4/problem16.py
@@ -1,1 +1,18 @@
-print("Hello World")
+def merge_intervals(intervals):
+    intervals.sort(key=lambda x: x[0])
+    merged = [intervals[0]]
+
+    for current in intervals[1:]:
+        last = merged[-1]
+
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged
+
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge_intervals(intervals))
+
